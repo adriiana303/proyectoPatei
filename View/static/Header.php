7d24e9f993f63c8c88d61a4d846c6b45c38aefc6
adriiana303 <50851503+adriiana303@users.noreply.github.com>
2019-05-21 00:39:27 -0600

    Update Header.php

--- View/static/Header.php
+++ View/static/Header.php
@@ -13,7 +13,8 @@
     <!-- Viewport Bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap Styles -->
-    <link rel="stylesheet" href="<?=$GLOBALS['COD']->dir?>/assets/css/bootstrap/bootstrap.min.css?<?=$elapsed_time?>">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link rel="stylesheet" href="<?=$GLOBALS['COD']->dir?>/assets/css/bootstrap/bootstrap.min.css?<?=$elapsed_time?>">-->
     <!-- Swiper -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css" > 
     <!-- Your Styles -->
