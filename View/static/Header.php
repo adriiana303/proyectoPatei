c2df371f73c4eeb1d04195142e7f76711b0cfd9f
Adriana Lopez <50851503+adriiana303@users.noreply.github.com>
2019-05-21 07:45:07 -0600

    Prueba2

--- View/static/Header.php
+++ View/static/Header.php
@@ -13,7 +13,8 @@
     <!-- Viewport Bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap Styles -->
-    <link rel="stylesheet" href="<?=$GLOBALS['COD']->dir?>/assets/css/bootstrap/bootstrap.min.css?<?=$elapsed_time?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="<?=$GLOBALS['COD']->dir?>/assets/css/bootstrap/bootstrap.min.css?<?=$elapsed_time?>"> -->
     <!-- Swiper -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css" > 
     <!-- Your Styles -->
